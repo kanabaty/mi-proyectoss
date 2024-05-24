5a72aea24fd3b5f0677ccc1f2e3437c002c895a9
kanabaty <kanabaty_91@hotmail.com>
2024-05-24 00:26:12 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('¡Excelente, mi amor! Sabía que tenías buenos gustos. Me haces muy feliz.');
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('Bueno, al cabo que ni quería, ¡era solo para una tarea! ¡Ja ja ja! :(');
+});
